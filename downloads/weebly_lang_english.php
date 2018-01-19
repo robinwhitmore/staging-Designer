0b56dab906f2e859eb176127bcd3af7e62e98f2f
robinwhitmore <rwhitmore@weebly.com>
2018-01-18 17:13:57 -0800

    various fixes found w/cloud

---
+++ downloads/weebly_lang_english.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * WHMCS Language File
+ * English (en)
+ */
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+$_LANG['isocode'] = 'en';
+
+/* WeeblyCloud langualge variables */
+$_LANG['WEEBLYCLOUD_PRODUCT_GROUP'] = "Weebly Plans";
+$_LANG['WEEBLYCLOUD_ADDON_CONFIG_DESC'] = "This addon allow you to configure Weebly API configuration.";
+$_LANG['WEEBLYCLOUD_BILLING_URL_ERR_MSG'] = "This is not a valid link. Please try again.";
+$_LANG['WEEBLYCLOUD_API_FAIL_ERR_MSG'] = "Weebly API error. Please check your weebly api credentials in WHMCS addon configuration.";
+$_LANG['WEEBLYCLOUD_SSO_ERR_MSG1'] = "Unable to generate Weebly SSO link. Weebly account/site is not created. Please contact your service provider.";
+$_LANG['WEEBLYCLOUD_DOMAIN_MISSING_ERR_MSG1'] = "Can not create Weebly site, Domain name is missing";
+$_LANG['WEEBLYCLOUD_DOMAIN_INVALID_ERR_MSG1'] = "Can not create Weebly site, Domain name is invalid";
+$_LANG['WEEBLYCLOUD_DOMAIN_INVALID_ERR_MSG2'] = "Please remove 'www.' from your domain before adding to your cart";
+$_LANG['WEEBLYCLOUD_INVALID_USERID'] = "UserId not found, exiting.";
+$_LANG['WEEBLYCLOUD_INVALID_SITEID'] = "SiteId not found, exiting";
+$_LANG['WEEBLYCLOUD_EMAIL_ALREADY_EXIST'] = "This email address already exists in Weebly Cloud. Please use a different email address.";
+$_LANG['WEEBLYCLOUD_DOMAIN_ALREADY_REGISTERED_IN_WEEBLY'] = "Domain already registered with Weebly";
+$_LANG['WEEBLYCLOUD_CUSTOM_FIELD_NAME'] = "Domain (Do not include &quot;www.&quot;)";
+$_LANG['WEEBLYCLOUD_EDIT_DOMAIN_WARNING_MSG'] = "Warning: Editing the domain name for a Weebly site can prevent the customer from publishing from the Weebly Editor.";
+$_LANG['WEEBLYCLOUD_DOMAIN_ALREADY_EXISTS_IN_WHMCS_DB'] = "Entered Domain is used by some other user. Please re-enter a valid Domain Name";
+$_LANG['WEEBLYCLOUD_UPGRADEURL_MATCH_MSG'] = "The upgrade URL in Weebly Cloud matches the WHMCS upgrade URL.";
+$_LANG['WEEBLYCLOUD_UPGRADEURL_NOMATCH_MSG'] = "The upgrade URL in Weebly Cloud doesn't match the WHMCS upgrade URL.";
+$_LANG['WEEBLYCLOUD_BILLINGURL_MATCH_MSG'] = "The billing URL in Weebly Cloud matches the WHMCS billing URL.";
+$_LANG['WEEBLYCLOUD_BILLINGURL_NOMATCH_MSG'] = "The billing URL in Weebly Cloud doesn't match the WHMCS billing URL.";
+$_LANG['WEEBLYCLOUD_PANEL_HEADING'] = "My Weebly Sites";
+$_LANG['WEEBLYCLOUD_PANEL_HEADING_NEWUSER'] = "Build A Stunning Website";
+$_LANG['WEEBLYCLOUD_PANEL_TEXT'] = "Create a unique website, online store or blog with this surprisingly easy site builder.";
+$_LANG['WEEBLYCLOUD_PANEL_GETSTARTED'] = "Get Started";
+$_LANG['WEEBLYCLOUD_PANEL_ADDSITE'] = "Add Site";
+$_LANG['WEEBLYCLOUD_PANEL_EDITSITE'] = "Edit Site";
+$_LANG['WEEBLYCLOUD_DIRECT_UPGRADE_ERROR'] = "You need to click 'Upgrade' from Weebly Cloud Admin to upgrade this plan";
+$_LANG['WEEBLYCLOUD_PLAN_TYPE_FREE'] = "Free";
+$_LANG['WEEBLYCLOUD_PLAN_TYPE_STARTER'] = "Starter";
+$_LANG['WEEBLYCLOUD_PLAN_TYPE_PRO'] = "Pro";
+$_LANG['WEEBLYCLOUD_PLAN_TYPE_BUSINESS'] = "Business";
